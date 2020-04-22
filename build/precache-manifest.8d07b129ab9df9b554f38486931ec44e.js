db77a31eb2f775a87492cf7f1b6a5e5784906f89
Vishal Singh <vishal.n.singh@ica.se>
2020-04-22 23:12:02 +0200

    added build

---
+++ build/precache-manifest.8d07b129ab9df9b554f38486931ec44e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2d5b566cb310e3c2360073df8d6db8f4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0d19302870584ed8ed43",
+    "url": "/static/css/2.68dd0589.chunk.css"
+  },
+  {
+    "revision": "f75878519d9c815fdcf1",
+    "url": "/static/css/main.2203b880.chunk.css"
+  },
+  {
+    "revision": "0d19302870584ed8ed43",
+    "url": "/static/js/2.1b1171bb.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.1b1171bb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f75878519d9c815fdcf1",
+    "url": "/static/js/main.2937315b.chunk.js"
+  },
+  {
+    "revision": "f483d1f7cd20d8e3322f",
+    "url": "/static/js/runtime-main.70302f69.js"
+  }
+]);
